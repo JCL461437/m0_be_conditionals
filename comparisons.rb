61bd36e7fa1e3448212b027ca4fd4a313dd5e108
Jan Lehigh <janlehigh461437@gmail.com>
2024-03-03 14:54:25 -0700

    Adds answers to Part 3

--- comparisons.rb
+++ comparisons.rb
@@ -100,16 +100,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
 # Assume that a dog is a puppy if it is less than 2 years old.
+puts loves_to_play && age < 2
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: The final line of code evaluated to true, because the boolean value for loves_to_play is true and integer value for age is 1, which is less than 2 which is the stipulated age that the dog must be less than in order to qualify as a puppy. 
